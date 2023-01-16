bfea6e6482844485201db573dd40a4b7f8f11c7a
PaulKoumedzro <paulinkoumedzro@gmail.com>
2023-01-16 09:57:49 -0600

    create message v2

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ updateClock();
 
 /*    MESSAGE GENERATOR    */
 
+let message = ['The journey of thousands miles begin with one step  —Lao Zhu',
+'All our dreams can come true, if we have the courage to pursue them. —Walt Disney',
+'The secret of getting ahead is getting started.  —Mark Twain','Do not limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve. —Mary Kay Ash',
+'The best time to plant a tree was 20 years ago. The second best time is now.” ―Chinese Proverb',
+'Only the paranoid survive.” —Andy Grove',
+'It is hard to beat a person who never gives up.” —Babe Ruth',
+'If people are doubting how far you can go, go so far that you cannot hear them anymore.” —Michele Ruiz',
+'Do what you feel in your heart to be right―for you will be criticized anyway. ―Eleanor Roosevelt',
+'You can either experience the pain of discipline or the pain of regret. The choice is yours.'
+,'Impossible is just an opinion. —Paulo Coelho'];
+
+
